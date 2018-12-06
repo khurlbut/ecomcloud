fa1dfe8e6e32d744c278e73581b4f3f8c68ed294
Kevin Hurlbut <kevinhurlbut@sbcglobal.net>
2018-12-05 18:32:57 -0800

    introduce user segmenter

--- ecom_test.go
+++ ecom_test.go
@@ -2,6 +2,7 @@ package ecomcloud_test
 
 import (
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	. "github.com/onsi/ginkgo"
@@ -17,47 +18,37 @@ var _ = Describe("Ecom Home Page Tests", func() {
 	})
 
 	It("should find the HOME upstream on requests to /", func() {
-		statusCode, body, err := request("http://13.66.226.127/", cookie)
+		statusCode, body, err := executeRequest("http://13.66.226.127/", cookie)
 		validate(statusCode, body, err)
 	})
 
 	It("should find the HOME upstream on requests to /browse/home.do", func() {
-		statusCode, body, err := request("http://13.66.226.127/browse/home.do", cookie)
+		statusCode, body, err := executeRequest("http://13.66.226.127/browse/home.do", cookie)
 		validate(statusCode, body, err)
 	})
 
 	It("should find the HOME upstream on requests to /browse/home.do/", func() {
-		statusCode, body, err := request("http://13.66.226.127/browse/home.do/", cookie)
+		statusCode, body, err := executeRequest("http://13.66.226.127/browse/home.do/", cookie)
 		validate(statusCode, body, err)
 	})
 
-	It("should match target on cookie found", func() {
-		client := &http.Client{}
-		req, _ := http.NewRequest("GET", "http://13.66.226.127/", nil)
-
-		req.AddCookie(cookie)
-		res, err := client.Do(req)
+	It("should go to USER-SEGMENTE on unknownShopperId cookie is not found", func() {
+		statusCode, body, err := executeRequest("http://13.66.226.127/browse/home.do/", nil)
 		Ω(err).ShouldNot(HaveOccurred())
 
-		body, _ := ioutil.ReadAll(res.Body)
-
-		Ω(res.StatusCode).Should(Equal(200))
-		Ω(string(body)).Should(Equal("HOME"))
+		Ω(statusCode).Should(Equal(200))
+		Ω(string(body)).Should(Equal("USER-SEGMENTER"))
 	})
 
-})
-
-var _ = Describe("Cookie Initialization Tests", func() {
-
-	XIt("should match target on cookie found", func() {
+	It("should be able to find the unknownShopperId cookie when other cookies are present", func() {
 		client := &http.Client{}
-		req, _ := http.NewRequest("GET", "http://13.66.226.127/", nil)
+		req := newRequest("GET", "http://13.66.226.127/")
 
 		cookieDecoy := &http.Cookie{Name: "cookie1", Value: "123"}
-		req.AddCookie(cookieDecoy)
-		cookie := &http.Cookie{Name: "unknownShopperId", Value: "123"}
-		req.AddCookie(cookie)
 		cookieDecoy2 := &http.Cookie{Name: "cookie2", Value: "123"}
+
+		req.AddCookie(cookieDecoy)
+		req.AddCookie(cookie)
 		req.AddCookie(cookieDecoy2)
 
 		res, err := client.Do(req)
@@ -68,12 +59,20 @@ var _ = Describe("Cookie Initialization Tests", func() {
 		Ω(res.StatusCode).Should(Equal(200))
 		Ω(string(body)).Should(Equal("HOME"))
 	})
+
 })
 
-func request(url string, cookie *http.Cookie) (int, []byte, error) {
+func newRequest(method string, url string) *http.Request {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		log.Printf("Error building request: %s", err.Error())
+	}
+	return req
+}
 
+func executeRequest(url string, cookie *http.Cookie) (int, []byte, error) {
 	client := &http.Client{}
-	req, _ := http.NewRequest("GET", url, nil)
+	req := newRequest("GET", url)
 	if cookie != nil {
 		req.AddCookie(cookie)
 	}
